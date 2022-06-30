225b1b2e36dc50ae9ebba9b03264feef23d0e39c
ca1c <gegs921@gmail.com>
2022-06-30 03:20:57 -0700

    options panel

--- src/components/boardpanel.component.js
+++ src/components/boardpanel.component.js
@@ -1,7 +1,8 @@
-import { Button, Grid, Stack, TextField } from '@mui/material';
+import { Button, Grid, Modal, Paper, Select, Stack, TextField, Typography } from '@mui/material';
 import Chessboard from 'chessboardjsx';
 import React, { Component } from 'react';
 import randPos from '../lib/randPos.js';
+import BOARD_THEMES from '../lib/boardThemes.js';
 
 class BoardPanel extends Component {
     constructor(props) {
@@ -9,10 +10,13 @@ class BoardPanel extends Component {
         this.state = {
             position: "start",
             pieceNum: "",
+            optionsOpen: false,
+            boardTheme: "default"
         }
 
         this.randomPosition = this.randomPosition.bind(this);
         this.resetPosition = this.resetPosition.bind(this);
+        this.toggleOptions = this.toggleOptions.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
@@ -26,6 +30,12 @@ class BoardPanel extends Component {
         this.setState({
             position: "start",
         })
+    }
+
+    toggleOptions() {
+        this.setState((prevState) => ({
+            optionsOpen: prevState.optionsOpen ? false : true,
+        }))
     }
 
     handleInputChange(e) {
@@ -52,10 +62,50 @@ class BoardPanel extends Component {
                         <Stack direction="row">
                             <Button onClick={this.randomPosition}>Start</Button>
                             <Button onClick={this.resetPosition}>Reset</Button>
-                            <TextField name="pieceNum" label="Piece Num" variant="outlined" onChange={this.handleInputChange}/>
+                            <Button onClick={this.toggleOptions}>Options</Button>
                         </Stack>
                     </Grid>
                 </Grid>
+                <Modal
+                    open={this.state.optionsOpen}
+                    onClose={this.toggleOptions}
+                >
+                    <Grid
+                            container
+                            direction="column"
+                            spacing={0}
+                            justifyContent="center"
+                            alignItems="center"
+                            style={{
+                                minHeight: '80vh',
+                            }}
+                    >
+                        <Grid item>
+                            <Paper sx={{
+                                padding: "30px",
+                                maxWidth: "900px",
+                                minHeight: "70vh",
+                                maxHeight: "70vh",
+                                position: "relative",
+                                margin: "0",
+                                overflow: "auto",
+                            }}
+                            >
+                                <Grid container direction="column" spacing={3}>
+                                    <Grid item>
+                                        <Typography variant="h3" sx={{textAlign: "center"}}>Options</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <TextField name="pieceNum" label="Piece Num" variant="outlined" value={this.state.pieceNum} onChange={this.handleInputChange}/>
+                                    </Grid>
+                                    <Grid item>
+                                        <TextField name="timerAmount" label="Timer Amount (s)" variant="outlined"/>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Modal>
             </div>
         )
     }
---
+++ src/lib/boardThemes.js
@@ -0,0 +1,8 @@
+const BOARD_THEMES = {
+    ruby: {
+        dark: 'rgb(245, 56, 87)',
+        light: 'rgb(250, 105, 129)',
+    }
+}
+
+export default BOARD_THEMES;
